91a759422c82075815fb593c52870f5bb7c9dfa7
jebediahelliott <jebediahelliott@gmail.com>
2019-04-07 18:44:44 -0400

    Add Services component and initial rendering test.

---
+++ src/components/Services.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Services from './Services';
+
+describe('<Services />', () => {
+  it('Renders a title', () => {
+    const wrapper = shallow(<Services />)
+    expect(wrapper.find('h1').text()).toMatch(/Services/)
+  })
+})
