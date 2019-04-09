69aacdf591dacab0a6556a7e83bc321046f33f3d
jebediahelliott <jebediahelliott@gmail.com>
2019-04-08 20:58:57 -0400

    Add initial Navbar styling. Remove default App styling.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 class Navbar extends Component {
 
   render() {
     return (
       <div id="navBar">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Log In</Link>
+        <Link to="/" className="link">Home</Link>
+        <Link to="/about" className="link">About</Link>
+        <Link to="/services" className="link">Services</Link>
+        <Link to="/contact" className="link">Contact</Link>
+        <Link to="/login" className="link">Log In</Link>
       </div>
     )
   }
