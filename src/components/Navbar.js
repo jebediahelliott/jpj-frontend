a2712301bb92cccbac951f3790ca66c45b6cb52b
jebediahelliott <jebediahelliott@gmail.com>
2019-04-11 10:05:22 -0400

    Add logo to navbar. Start adjustsing size and alignment.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from './images/logo.jpg'
 
 class Navbar extends Component {
 
   render() {
     return (
       <div id="navBar">
+        <img src={ logo } alt="Logo" className="logo" />
         <Link to="/" className="link">Home</Link>
         <Link to="/about" className="link">About</Link>
         <Link to="/services" className="link">Services</Link>
