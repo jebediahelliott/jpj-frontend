ef997e8d59c3e5677b852674e492983dcc6809c8
jebediahelliott <jebediahelliott@gmail.com>
2019-04-14 12:39:45 -0400

    Add login form, bootsrap, and react-bootrap.

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -5,6 +5,11 @@ import Login from './Login';
 describe('<Login />', () => {
   it('Has a title', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find('h1').text()).toMatch(/Log In/);
+    expect(wrapper.find('h1').text()).toEqual(expect.stringMatching(/Log In/));
+  })
+  it('Has a form with email and password', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find('Form').text()).toEqual(expect.stringMatching('Email'))
+    expect(wrapper.find('Form').text()).toEqual(expect.stringMatching('Password'))
   })
 })
