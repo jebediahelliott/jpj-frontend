1e66a131ffbab671f28611f00315ac80f408f1b0
jebediahelliott <jebediahelliott@gmail.com>
2019-04-15 22:11:36 -0400

    Add styling to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './Layout.css'
+import './Footer.css'
 
 class Footer extends Component {
 
   render() {
     return (
       <div className="footer">
-        <span>John P. Jones</span>
-        <span>814-769-1883</span>
-        <span>eichenhalle@aol.com</span>
-        <a id="faceLink" href="https://www.facebook.com/John-P-Jones-Canine-Training-270551002841/">Facebook</a>
+        <div className="footer-content">
+          <span>&copy; John P. Jones Canine Training</span>
+          <span>814-769-1883</span>
+          <span>eichenhalle@aol.com</span>
+          <a id="faceLink" href="https://www.facebook.com/John-P-Jones-Canine-Training-270551002841/">Facebook</a>
+        </div>
       </div>
     )
   }
