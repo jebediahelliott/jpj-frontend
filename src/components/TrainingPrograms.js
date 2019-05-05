7ae6f7db456d2efef741d564dbf4a9c7e4d77ab9
jebediahelliott <jebediahelliott@gmail.com>
2019-05-05 12:52:48 -0400

    Add TrainingPrograms and tests

---
+++ src/components/TrainingPrograms.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './Layout.css'
+
+
+class TrainingPrograms extends Component {
+
+
+
+  render() {
+    return(
+      <div className='staticPage'>
+        <h1>{this.props.page.Title}</h1>
+        <ReactMarkdown source={this.props.page.Content} />
+      </div>
+    )
+  }
+
+}
+
+export default TrainingPrograms;
---
+++ src/components/TrainingPrograms.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TrainingPrograms from './TrainingPrograms';
+
+describe('<TrainingPrograms />', () => {
+  const props = {
+    Title: 'TrainingPrograms',
+    Content: 'This is the services content'
+  }
+  it('Renders a title', () => {
+    const wrapper = mount(<TrainingPrograms page={props} />);
+    expect(wrapper.find('h1').text()).toEqual(props.Title);
+    expect(wrapper.find('p').text()).toEqual(props.Content);
+  })
+})
