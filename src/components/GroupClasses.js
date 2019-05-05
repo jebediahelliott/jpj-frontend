a6c9e92a154deb18b91a51af5a1ab6d4ebecf40b
jebediahelliott <jebediahelliott@gmail.com>
2019-05-05 12:56:43 -0400

    Add GroupClasses and tests.

---
+++ src/components/GroupClasses.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown'
+import './Layout.css';
+
+class GroupClasses extends Component {
+
+
+
+  render() {
+    return(
+      <div className='staticPage'>
+        <h1>{this.props.page.Title}</h1>
+        <ReactMarkdown source={this.props.page.Content} />
+      </div>
+    )
+  }
+
+}
+
+export default GroupClasses;
