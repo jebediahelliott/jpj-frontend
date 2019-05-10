aaa5cca34f289ac99a6a928ca23857387b7ae019
jebediahelliott <jebediahelliott@gmail.com>
2019-05-10 15:22:12 -0400

    Move links from TrainingLinks into Navbar and remove TrainingLinks. Add dropdown to for new links.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import TrainingLinks from '../../containers/TrainingLinks/TrainingLinks';
 import './Navbar.css';
 
 class Navbar extends Component {
@@ -9,26 +10,50 @@ class Navbar extends Component {
       return (
         <div>
           <div className="banner"></div>
-          <div id="navBar">
-            <NavLink to="/" className="link" activeStyle={{color: '#F1CD4E'}} exact>Home</NavLink>
-            <NavLink to="/about" className="link" activeStyle={{color: '#F1CD4E'}} exact>About</NavLink>
-            <NavLink to="/training-programs" className="link" activeStyle={{color: '#F1CD4E'}} exact>Training Programs</NavLink>
-            <NavLink to="/contact" className="link" activeStyle={{color: '#F1CD4E'}} exact>Contact</NavLink>
-            <NavLink to="/" className="link" onClick={this.props.handleLogout} activeStyle={{color: '#F1CD4E'}} exact>Log Out</NavLink>
-          </div>
+          <nav id="navBar">
+            <ul className="menu">
+              <li><NavLink to="/" className="link" activeStyle={{color: '#F1CD4E'}} exact>Home</NavLink></li>
+              <li><NavLink to="/about" className="link" activeStyle={{color: '#F1CD4E'}} exact>About</NavLink></li>
+              <li id="droplink"><NavLink to="/training-programs" className="link" activeStyle={{color: '#F1CD4E'}} exact>Training Programs</NavLink>
+                <ul>
+                  <li><NavLink to="/consultation" className="" activeStyle={{color: '#F1CD4E'}} exact>Consultation</NavLink></li>
+                  <li><NavLink to="/puppy-school" className="" activeStyle={{color: '#F1CD4E'}} exact>Puppy School</NavLink></li>
+                  <li><NavLink to="/basic-program" className="" activeStyle={{color: '#F1CD4E'}} exact>Basic Program</NavLink></li>
+                  <li><NavLink to="/group-classes" className="" activeStyle={{color: '#F1CD4E'}} exact>Group Classes</NavLink></li>
+                  <li><NavLink to="/graduate-program" className="" activeStyle={{color: '#F1CD4E'}} exact>Graduate Program</NavLink></li>
+                  <li><NavLink to="/resident-training" className="" activeStyle={{color: '#F1CD4E'}} exact>Resident Training</NavLink></li>
+                  <li><NavLink to="/tracking" className="" activeStyle={{color: '#F1CD4E'}} exact>Tracking</NavLink></li>
+                </ul>
+              </li>
+              <li><NavLink to="/contact" className="link" activeStyle={{color: '#F1CD4E'}} exact>Contact</NavLink></li>
+              <li><NavLink to="/login" className="link" activeStyle={{color: '#F1CD4E'}} exact>Log In</NavLink></li>
+            </ul>
+          </nav>
         </div>
       )
     }
     return (
       <div>
         <div className="banner"></div>
-        <div id="navBar">
-          <NavLink to="/" className="link" activeStyle={{color: '#F1CD4E'}} exact>Home</NavLink>
-          <NavLink to="/about" className="link" activeStyle={{color: '#F1CD4E'}} exact>About</NavLink>
-          <NavLink to="/training-programs" className="link" activeStyle={{color: '#F1CD4E'}} exact>Training Programs</NavLink>
-          <NavLink to="/contact" className="link" activeStyle={{color: '#F1CD4E'}} exact>Contact</NavLink>
-          <NavLink to="/login" className="link" activeStyle={{color: '#F1CD4E'}} exact>Log In</NavLink>
-        </div>
+        <nav id="navBar">
+          <ul className="menu">
+            <li><NavLink to="/" className="link" activeStyle={{color: '#F1CD4E'}} exact>Home</NavLink></li>
+            <li><NavLink to="/about" className="link" activeStyle={{color: '#F1CD4E'}} exact>About</NavLink></li>
+            <li id="droplink"><NavLink to="/training-programs" className="link" activeStyle={{color: '#F1CD4E'}} exact>Training Programs</NavLink>
+              <ul className="dropdown">
+                <li><NavLink to="/consultation" className="link" activeStyle={{color: '#F1CD4E'}} exact>Consultation</NavLink></li>
+                <li><NavLink to="/puppy-school" className="link" activeStyle={{color: '#F1CD4E'}} exact>Puppy School</NavLink></li>
+                <li><NavLink to="/basic-program" className="link" activeStyle={{color: '#F1CD4E'}} exact>Basic Program</NavLink></li>
+                <li><NavLink to="/group-classes" className="link" activeStyle={{color: '#F1CD4E'}} exact>Group Classes</NavLink></li>
+                <li><NavLink to="/graduate-program" className="link" activeStyle={{color: '#F1CD4E'}} exact>Graduate Program</NavLink></li>
+                <li><NavLink to="/resident-training" className="link" activeStyle={{color: '#F1CD4E'}} exact>Resident Training</NavLink></li>
+                <li><NavLink to="/tracking" className="link" activeStyle={{color: '#F1CD4E'}} exact>Tracking</NavLink></li>
+              </ul>
+            </li>
+            <li><NavLink to="/contact" className="link" activeStyle={{color: '#F1CD4E'}} exact>Contact</NavLink></li>
+            <li><NavLink to="/login" className="link" activeStyle={{color: '#F1CD4E'}} exact>Log In</NavLink></li>
+          </ul>
+        </nav>
       </div>
     )
   }
