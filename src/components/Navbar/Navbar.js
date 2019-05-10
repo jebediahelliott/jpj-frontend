8917f741ba1f20ec8a3b2b02e0c149ac96e811a4
jebediahelliott <jebediahelliott@gmail.com>
2019-05-10 15:29:11 -0400

    Fix active color for dropdown links. Chnage h1 to bold.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -41,13 +41,13 @@ class Navbar extends Component {
             <li><NavLink to="/about" className="link" activeStyle={{color: '#F1CD4E'}} exact>About</NavLink></li>
             <li id="droplink"><NavLink to="/training-programs" className="link" activeStyle={{color: '#F1CD4E'}} exact>Training Programs</NavLink>
               <ul className="dropdown">
-                <li><NavLink to="/consultation" className="link" activeStyle={{color: '#F1CD4E'}} exact>Consultation</NavLink></li>
-                <li><NavLink to="/puppy-school" className="link" activeStyle={{color: '#F1CD4E'}} exact>Puppy School</NavLink></li>
-                <li><NavLink to="/basic-program" className="link" activeStyle={{color: '#F1CD4E'}} exact>Basic Program</NavLink></li>
-                <li><NavLink to="/group-classes" className="link" activeStyle={{color: '#F1CD4E'}} exact>Group Classes</NavLink></li>
-                <li><NavLink to="/graduate-program" className="link" activeStyle={{color: '#F1CD4E'}} exact>Graduate Program</NavLink></li>
-                <li><NavLink to="/resident-training" className="link" activeStyle={{color: '#F1CD4E'}} exact>Resident Training</NavLink></li>
-                <li><NavLink to="/tracking" className="link" activeStyle={{color: '#F1CD4E'}} exact>Tracking</NavLink></li>
+                <li><NavLink to="/consultation" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Consultation</NavLink></li>
+                <li><NavLink to="/puppy-school" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Puppy School</NavLink></li>
+                <li><NavLink to="/basic-program" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Basic Program</NavLink></li>
+                <li><NavLink to="/group-classes" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Group Classes</NavLink></li>
+                <li><NavLink to="/graduate-program" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Graduate Program</NavLink></li>
+                <li><NavLink to="/resident-training" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Resident Training</NavLink></li>
+                <li><NavLink to="/tracking" className="link" activeStyle={{color: 'rgb(132,14,5)'}} exact>Tracking</NavLink></li>
               </ul>
             </li>
             <li><NavLink to="/contact" className="link" activeStyle={{color: '#F1CD4E'}} exact>Contact</NavLink></li>
