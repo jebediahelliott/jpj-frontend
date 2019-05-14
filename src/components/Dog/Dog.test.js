0b30b6a2161f5b40e5256461fe09669d83cb9b7c
jebediahelliott <jebediahelliott@gmail.com>
2019-05-14 14:47:40 -0400

    Update Dog test

--- src/components/Dog/Dog.test.js
+++ src/components/Dog/Dog.test.js
@@ -4,12 +4,20 @@ import Dog from './Dog';
 
 describe('<Dog />', () => {
   it('Greets the user', () => {
-    const props = {
-      name: 'Arya',
-      notes: 'Barks at guests'
-    }
-    const wrapper = shallow(<Dog dog={props}/>);
-    expect(wrapper.find('h1').text()).toEqual(props.name);
-    expect(wrapper.find('p').text()).toEqual(props.notes);
+    const props = [
+      {
+        id: 1,
+        name: "Arya",
+        notes: "Spoiled rotten",
+        picture: [
+          {
+            url: "/uploads/6d83cf94c1fd44c58e05f136e54c0845.jpg"
+          }
+        ]
+      }
+    ]
+    const wrapper = shallow(<Dog dogs={props}/>);
+    expect(wrapper.find('h1').text()).toEqual(`${props[0].name}`);
+    expect(wrapper.find('p').text()).toEqual(props[0].notes);
   })
 })
