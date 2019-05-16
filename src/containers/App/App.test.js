6909b70d3f1e3fff3ef52bdba6612ee6a5fcce61
jebediahelliott <jebediahelliott@gmail.com>
2019-05-16 18:31:11 -0400

    Add test for fetchPages in App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,32 +3,56 @@ import { shallow, mount } from 'enzyme';
 import App from './App';
 import axios from 'axios';
 
-// jest.mock('axios');
-
 describe('<App />', () => {
   it('renders a container for the App', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('.App').length).toEqual(1);
   })
   it('calls fetchPages when it mounts', () => {
-    const pages = [
-      {
-        title: 'About',
-        content: 'About content'
-      },
-      {
-        title: 'Training Programs',
-        content: 'Training content'
-      }
-    ];
-    const resp = {data: pages};
     const wrapper = shallow(<App />);
     const instance = wrapper.instance()
     const spy = jest.spyOn(instance, 'fetchPages');
     instance.componentDidMount();
-    // const instance = wrapper.instance();
-    // axios.get.mockResolvedValue(resp);
-    expect(spy).toBeCalled();
 
+    expect(spy).toBeCalled();
+  })
+  describe('fetchPages', () => {
+    it('makes a GET request', async () => {
+      const pages = [
+        {
+          title: 'About',
+          content: 'About content'
+        },
+        {
+          title: 'Training Programs',
+          content: 'Training content'
+        }
+      ];
+      const resp = {data: pages};
+      const wrapper = shallow(<App />);
+      const instance = wrapper.instance()
+      const getMock = jest.spyOn(axios, 'get');
+      getMock.mockResolvedValue(resp)
+      await instance.componentDidMount();
+      expect(getMock).toHaveBeenCalled();
+      expect(wrapper.state()).toEqual(
+        {
+          home: { title: 'About', content: 'About content' },
+          about: { title: 'Training Programs', content: 'Training content' },
+          trainingPrograms: undefined,
+          groupClasses: undefined,
+          graduateProgram: undefined,
+          tracking: undefined,
+          residentTraining: undefined,
+          consultation: undefined,
+          basicProgram: undefined,
+          puppySchool: undefined,
+          token: undefined,
+          user: undefined,
+          dogs: undefined
+        }
+      )
+      // expect(spy).toBeCalled();
+    })
   })
 })
