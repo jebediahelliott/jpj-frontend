def18a61f23061e42e3ea53704bf26f6cc5e5c2a
jebediahelliott <jebediahelliott@gmail.com>
2019-05-17 21:48:00 -0400

    Refactor About tests

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -3,13 +3,16 @@ import { mount } from 'enzyme';
 import About from './About';
 
 describe('<About />', () => {
+  const props = {
+    title: 'About',
+    content: 'This is content for the About page.'
+  }
   it('Renders a title', () => {
-    const props = {
-      title: 'About',
-      content: 'This is content for the About page.'
-    }
     const wrapper = mount(<About page={props} />);
     expect(wrapper.find('h1').text()).toEqual(props.title);
+  })
+  it('Renders page content', () => {
+    const wrapper = mount(<About page={props} />);
     expect(wrapper.find('p').text()).toEqual(props.content);
   })
 })
