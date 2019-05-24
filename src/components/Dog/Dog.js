83fc6061bff4dbfce83f60466fc0e85c7977ce4f
jebediahelliott <jebediahelliott@gmail.com>
2019-05-24 16:30:13 -0400

    Update Profile styling

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -10,8 +10,9 @@ class Dog extends Component {
       source += this.props.dogs[0].picture[0].url
       return (
         <div>
-          <h1>{this.props.dogs[0].name}</h1>
+          <h2>{this.props.dogs[0].name}</h2>
           <img src={source} alt={this.props.dogs[0].name} />
+          <h3>Traning Notes</h3>
           <p>{this.props.dogs[0].notes}</p>
         </div>
       )
