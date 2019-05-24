0cce9710a83b35a415fb56b933dc045876b19745
jebediahelliott <jebediahelliott@gmail.com>
2019-05-23 22:02:20 -0400

    Stub out ServiceTraining and test

---
+++ src/components/ServiceTraining/ServiceTraining.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import '../Layout.css'
+
+
+class ServiceTraining extends Component {
+
+
+
+  render() {
+    return(
+      <div className='staticPage'>
+        <h1>{this.props.page.title}</h1>
+        <ReactMarkdown source={this.props.page.content} />
+      </div>
+    )
+  }
+
+}
+
+export default ServiceTraining;
