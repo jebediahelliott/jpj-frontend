b6e7c513f3cef8c799dce542e55fdcf6e34e6ed1
jebediahelliott <jebediahelliott@gmail.com>
2019-06-01 22:35:20 -0400

    Adjust Navbar test to reflect removal of login link

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -5,6 +5,6 @@ import Navbar from './Navbar';
 describe('<Navbar />', () => {
   it('Has a link for each page and a log in link', () => {
     const wrapper = shallow(<Navbar.WrappedComponent />);
-    expect(wrapper.find('NavLink').length).toEqual(12);
+    expect(wrapper.find('NavLink').length).toEqual(11);
   })
 })
