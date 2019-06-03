431e4b164fc24732d492c48376744f7dd6e267c4
jebediahelliott <jebediahelliott@gmail.com>
2019-06-03 13:50:57 -0400

    Add photos

--- src/components/GraduateProgram/GraduateProgram.js
+++ src/components/GraduateProgram/GraduateProgram.js
@@ -9,6 +9,7 @@ class GraduateProgram extends Component {
   render() {
     return(
       <div className='staticPage'>
+      <h1>Graduaate Program</h1>
         <p>
           For those who would like to establish more control with their dogs, the Graduate Program takes the Basic Program a couple of steps further and teaches the finish, stand for exam, and sit, stand, and down in motion. The Graduate Program cositsts of 7 private lessons and 5 group lessons. For dogs 4 months and older of any breed.
         </p>
